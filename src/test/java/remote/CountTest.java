441f326728030160ce1680208f578fd924403e6e
Manish Rastogi <manishrastogi@MVYVPXJFXQ.local>
2024-10-26 23:51:07 +0530

    second commit

---
+++ src/test/java/remote/CountTest.java
@@ -0,0 +1,21 @@
+package remote;
+
+import org.testng.annotations.Test;
+
+import java.time.LocalDateTime;
+
+public class CountTest {
+
+    count c = new count();
+@Test
+    public void tc1()
+{
+   int startTime =  LocalDateTime.now().getNano();
+    System.out.println("StartTime = "+ startTime);
+    System.out.println(c.allCount(10));
+    int endTime = LocalDateTime.now().getNano();
+    System.out.println("EndTime ="+ endTime);
+    int interval = endTime - startTime;
+    System.out.println(interval);
+}
+}
