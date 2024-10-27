267202d7a133a1c0214b416342bc542326bf2566
Manish Rastogi <manishrastogi@MVYVPXJFXQ.local>
2024-10-27 20:31:53 +0530

    recurssion

---
+++ src/test/java/remote/RecurssionTest.java
@@ -0,0 +1,16 @@
+package remote;
+
+import org.testng.annotations.Test;
+
+  public class RecurssionTest {
+
+    Recurssion rc = new Recurssion();
+
+    @Test
+    public void test1()
+    {
+        rc.printcount(10);
+    }
+}
+
+
